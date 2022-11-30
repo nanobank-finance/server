255bd9263a94edb2bd1f8ba3d4534ac0d0384b18
Nate Schultz <ngs32@drexel.edu>
2022-11-29 19:10:28 -0500

    fastapi init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # server
+
+Setup: `python3.9 pip install -r requirements.txt`
+
+Run: `cd src; uvicorn main:app --reload`
+
+Local API docs: http://127.0.0.1:8000/docs#/ or http://127.0.0.1:8000/redoc
+
---
+++ src/main.py
@@ -0,0 +1,19 @@
+from typing import Union
+from fastapi import FastAPI
+import schemas
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: schemas.Item):
+    return {"item_name": item.name, "item_id": item_id}

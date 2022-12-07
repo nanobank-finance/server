da5182a2476fe50d53e60cb75891843dc32175e3
Nate Schultz <nate.schultz@outlook.com>
2022-12-07 16:35:50 -0500

    added response models

--- src/main.py
+++ src/main.py
@@ -8,72 +8,79 @@ from firebase_admin import firestore
 db = firestore.client()
 app = FastAPI()
 
-""" loan create/read/update/delete endpoints """
+loan_ref = db.collection(u'loan')
 
-@app.post("/loan")
+""" loan application create/read/update/delete endpoints """
+
+@app.post("/loan", response_model=schemas.SuccessOrFailResponse)
 async def submit_loan_application(user = Depends(auth.get_user_token), application: schemas.LoanApplication):
     return {"Hello": "World"}
 
-@app.get("/loans")
+@app.get("/loans", response_model=List[schemas.LoanApplication])
 async def get_loan_list(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/loan/{loan_id}")
+@app.get("/loan/{loan_id}", response_model=schemas.LoanApplication)
 async def get_loan_details(user = Depends(auth.get_user_token), loan_id: int):
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f'Document data: {doc.to_dict()}')
+    else:
+        print(u'No such document!')
     return {"Hello": "World"}
 
-@app.put("/loan/{loan_id}")
-async def update_loan(user = Depends(auth.get_user_token), loan_id: int):
+@app.put("/loan/{loan_id}", response_model=schemas.SuccessOrFailResponse)
+async def update_loan_application(user = Depends(auth.get_user_token), application: schemas.LoanApplication):
     return {"Hello": "World"}
 
-@app.delete("/loan/{loan_id}")
+@app.delete("/loan/{loan_id}", response_model=schemas.SuccessOrFailResponse)
 async def withdraw_loan_application(user = Depends(auth.get_user_token), loan_id: int):
     return {"Hello": "World"}
 
 """ public loans endpoint for anonymized data """
 
-@app.get("/loans")
+@app.get("/loans", response_model=List[schemas.LoanApplication])
 async def get_public_loans():
     return {"Hello": "World"}
 
 """ run various types of background checks as part of the application process """
 
-@app.get("/check/{type}")
+@app.get("/check/{type}", response_model=schemas.SuccessOrFailResponse)
 async def start_user_kyc(user = Depends(auth.get_user_token), type: schemas.checkType):
     return {"Hello": "World"}
 
 """ savings account endpoints """
 
-@app.get("/wallet/savings")
+@app.get("/wallet/savings", response_model=schemas.Wallet)
 async def get_savings_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/savings/interest")
+@app.get("/savings/interest", response_model=schemas.SavingsInterest)
 async def get_interest_payment_history(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
 """ other wallet/payment related endpoints """
 
-@app.get("/wallet/checking")
+@app.get("/wallet/checking", response_model=schemas.Wallet)
 async def get_checking_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/wallet/payment")
+@app.get("/wallet/payment", response_model=schemas.Wallet)
 async def get_bill_pay_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/withdaw/savings")
+@app.get("/withdaw/savings", response_model=schemas.SuccessOrFailResponse)
 async def withdraw_from_savings(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/withdaw/checking")
+@app.get("/withdaw/checking", response_model=schemas.SuccessOrFailResponse)
 async def withdraw_from_checking(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/transfer/savings")
+@app.get("/transfer/savings", response_model=schemas.SuccessOrFailResponse)
 async def transfer_from_savings_to_checking(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/transfer/checking")
+@app.get("/transfer/checking", response_model=schemas.SuccessOrFailResponse)
 async def transfer_from_checking_to_savings(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
--- src/schemas.py
+++ src/schemas.py
@@ -2,16 +2,47 @@ from typing import Union
 from pydantic import BaseModel
 from enum import Enum
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-class LoanApplication(BaseModel):
-    name: str
-
 class CheckType(Enum):
     CREDIT_CHECK: 1
     PROOF_OF_INCOME_CHECK: 2
     BACKGROUND_CHECK: 3
     IDENTITY_VERIFICATION_CHECK: 4
+
+class SuccessOrFailResponse(BaseModel):
+    success: bool
+    error_message: Union[str, None] = None
+
+class Wallet(object):
+    def __init__(self):
+        pass
+
+class SavingsInterest(object):
+    def __init__(self):
+        pass
+
+class LoanApplication(object):
+    def __init__(self, name, state, country, capital=False, population=0, regions=[]):
+        self.name = name
+        self.state = state
+        self.country = country
+        self.capital = capital
+        self.population = population
+        self.regions = regions
+
+    @staticmethod
+    def from_dict(source):
+        pass
+
+    def to_dict(self):
+        pass
+
+    def __repr__(self):
+        return (
+            f'City(\
+                name={self.name}, \
+                country={self.country}, \
+                population={self.population}, \
+                capital={self.capital}, \
+                regions={self.regions}\
+            )'
+        )

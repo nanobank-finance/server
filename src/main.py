dcd84849bf64846cd92a3869006572aef5a0f08e
Nate Schultz <nate.schultz@outlook.com>
2022-12-07 15:39:40 -0500

    worked on adding needed endpoints

--- src/main.py
+++ src/main.py
@@ -7,15 +7,74 @@ from firebase_admin import firestore
 db = firestore.client()
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+# todo: request decorator to check id_token and mark private vs public endpoints easier
+
+""" loan create/read/update/delete endpoints """
+
+@app.post("/user/{uid}/loan")
+async def submit_loan_application(uid: str, id_token: str, application: schemas.LoanApplication):
     return {"Hello": "World"}
 
+@app.get("/user/{uid}/loans")
+async def get_loan_list(uid: str, id_token: str):
+    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/user/{uid}/loan/{loan_id}")
+async def get_loan_details(uid: str, loan_id: int, id_token: str):
+    return {"Hello": "World"}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: schemas.Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/user/{uid}/loan/{loan_id}")
+async def update_loan(uid: str, loan_id: int, id_token: str):
+    return {"Hello": "World"}
+
+@app.delete("/user/{uid}/loan/{loan_id}")
+async def withdraw_loan_application(uid: str, loan_id: int, id_token: str):
+    return {"Hello": "World"}
+
+""" public loans endpoint for anonymized data """
+
+@app.get("/loans")
+async def get_public_loans():
+    return {"Hello": "World"}
+
+""" run various types of background checks as part of the application process """
+
+@app.get("/user/{uid}/check/{type}")
+async def start_user_kyc(uid: str, type: schemas.checkType, id_token: str):
+    return {"Hello": "World"}
+
+""" savings account endpoints """
+
+@app.get("/user/{uid}/savings")
+async def get_savings_wallet(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/savings/interest")
+async def get_interest_payment_history(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+""" other wallet/payment related endpoints """
+
+@app.get("/user/{uid}/checking")
+async def get_checking_wallet(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/pay")
+async def get_bill_pay_wallet(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/withdaw/savings")
+async def withdraw_from_savings(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/withdaw/checking")
+async def withdraw_from_checking(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/transfer/savings")
+async def transfer_from_savings_to_checking(uid: str, id_token: str):
+    return {"Hello": "World"}
+
+@app.get("/user/{uid}/transfer/checking")
+async def transfer_from_checking_to_savings(uid: str, id_token: str):
+    return {"Hello": "World"}
--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,17 @@
 from typing import Union
 from pydantic import BaseModel
+from enum import Enum
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
+class LoanApplication(BaseModel):
+    name: str
+
+class CheckType(Enum):
+    CREDIT_CHECK: 1
+    PROOF_OF_INCOME_CHECK: 2
+    BACKGROUND_CHECK: 3
+    IDENTITY_VERIFICATION_CHECK: 4

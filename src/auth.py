b618f187580c8d7aa83c0759cb825135e37ec377
Nate Schultz <nate.schultz@outlook.com>
2022-12-07 16:13:37 -0500

    added jwt token verification

---
+++ src/auth.py
@@ -0,0 +1,21 @@
+from firebase_admin import firestore, auth, credentials, initialize_app
+
+initialize_app(credentials.Certificate('./key.json'))
+
+def get_user_token(res: Response, credential: HTTPAuthorizationCredentials=Depends(HTTPBearer(auto_error=False))):
+    if credential is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Bearer authentication is needed",
+            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+        )
+    try:
+        decoded_token = auth.verify_id_token(credential.credentials)
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication from Firebase. {err}",
+            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+        )
+    res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+    return decoded_token
--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
 from typing import Union
 from fastapi import FastAPI
 import schemas
+import auth
 import firebase_admin
 from firebase_admin import firestore
 
 db = firestore.client()
 app = FastAPI()
 
-# todo: request decorator to check id_token and mark private vs public endpoints easier
-
 """ loan create/read/update/delete endpoints """
 
-@app.post("/user/{uid}/loan")
-async def submit_loan_application(uid: str, id_token: str, application: schemas.LoanApplication):
+@app.post("/loan")
+async def submit_loan_application(user = Depends(auth.get_user_token), application: schemas.LoanApplication):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/loans")
-async def get_loan_list(uid: str, id_token: str):
+@app.get("/loans")
+async def get_loan_list(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/loan/{loan_id}")
-async def get_loan_details(uid: str, loan_id: int, id_token: str):
+@app.get("/loan/{loan_id}")
+async def get_loan_details(user = Depends(auth.get_user_token), loan_id: int):
     return {"Hello": "World"}
 
-@app.put("/user/{uid}/loan/{loan_id}")
-async def update_loan(uid: str, loan_id: int, id_token: str):
+@app.put("/loan/{loan_id}")
+async def update_loan(user = Depends(auth.get_user_token), loan_id: int):
     return {"Hello": "World"}
 
-@app.delete("/user/{uid}/loan/{loan_id}")
-async def withdraw_loan_application(uid: str, loan_id: int, id_token: str):
+@app.delete("/loan/{loan_id}")
+async def withdraw_loan_application(user = Depends(auth.get_user_token), loan_id: int):
     return {"Hello": "World"}
 
 """ public loans endpoint for anonymized data """
@@ -39,42 +38,42 @@ async def get_public_loans():
 
 """ run various types of background checks as part of the application process """
 
-@app.get("/user/{uid}/check/{type}")
-async def start_user_kyc(uid: str, type: schemas.checkType, id_token: str):
+@app.get("/check/{type}")
+async def start_user_kyc(user = Depends(auth.get_user_token), type: schemas.checkType):
     return {"Hello": "World"}
 
 """ savings account endpoints """
 
-@app.get("/user/{uid}/savings")
-async def get_savings_wallet(uid: str, id_token: str):
+@app.get("/wallet/savings")
+async def get_savings_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/savings/interest")
-async def get_interest_payment_history(uid: str, id_token: str):
+@app.get("/savings/interest")
+async def get_interest_payment_history(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
 """ other wallet/payment related endpoints """
 
-@app.get("/user/{uid}/checking")
-async def get_checking_wallet(uid: str, id_token: str):
+@app.get("/wallet/checking")
+async def get_checking_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/pay")
-async def get_bill_pay_wallet(uid: str, id_token: str):
+@app.get("/wallet/payment")
+async def get_bill_pay_wallet(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/withdaw/savings")
-async def withdraw_from_savings(uid: str, id_token: str):
+@app.get("/withdaw/savings")
+async def withdraw_from_savings(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/withdaw/checking")
-async def withdraw_from_checking(uid: str, id_token: str):
+@app.get("/withdaw/checking")
+async def withdraw_from_checking(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/transfer/savings")
-async def transfer_from_savings_to_checking(uid: str, id_token: str):
+@app.get("/transfer/savings")
+async def transfer_from_savings_to_checking(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}
 
-@app.get("/user/{uid}/transfer/checking")
-async def transfer_from_checking_to_savings(uid: str, id_token: str):
+@app.get("/transfer/checking")
+async def transfer_from_checking_to_savings(user = Depends(auth.get_user_token)):
     return {"Hello": "World"}

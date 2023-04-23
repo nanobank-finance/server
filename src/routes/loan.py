cd16b212ecde5b903a62cdb88bbeb297552509db
Nate Schultz <nate.schultz@outlook.com>
2023-04-23 12:29:57 -0400

    worked on get_borrower_loan_applications

--- src/routes/loan.py
+++ src/routes/loan.py
@@ -1,10 +1,13 @@
 from fastapi import Depends
 from typing import List
+import json
 from src.schemas import SuccessOrFailResponse
 from src.utils import get_user_token
 from ipfsclient.ipfs import Ipfs
+from ipfskvs.store import Store
 from bizlogic.loan.reader import LoanReader
-from bizlogic.application.reader import LoanApplicationReader
+from bizlogic.loan.writer import LoanWriter
+from bizlogic.application import LoanApplicationReader, LoanApplicationWriter
 
 class LoanRouter():
 
@@ -13,16 +16,62 @@ class LoanRouter():
         ipfsclient = Ipfs()
         loan_reader = LoanReader(ipfsclient)
         loan_application_reader = LoanApplicationReader(ipfsclient)
+        # loan_writer = LoanWriter(ipfsclient)
+
 
         # Loan application endpoints
 
         @app.post("/loan/application", response_model=SuccessOrFailResponse)
-        async def submit_loan_application(user = Depends(get_user_token)):
-            return {"Hello": "World"}
+        async def submit_loan_application(asking: int): #, user = Depends(get_user_token)):
+            borrower = "123"  # TODO: get from KYC
+            try:
+                loan_application_writer = LoanApplicationWriter(ipfsclient, borrower, asking)
+                loan_application_writer.write()
+            except Exception as e:
+                return SuccessOrFailResponse(
+                    success=False,
+                    error_message=e
+                )
+
+            return SuccessOrFailResponse(
+                success=True
+            )
+        
+        @app.get("/loan/application")
+        async def get_borrower_loan_applications(user = Depends(get_user_token)):
+            borrower = "123"  # TODO: get from KYC
+            try:
+                results = loan_application_reader.get_loan_applications_for_borrower(borrower)
+            except Exception as e:
+                return SuccessOrFailResponse(
+                    success=False,
+                    error_message=e
+                )
+
+            return json.loads(Store.to_dataframe(results, protobuf_parsers={
+                "amount_asking": lambda store: store.reader.amount_asking,
+                "closed": lambda store: store.reader.closed,
+            }).to_json())
 
         @app.delete("/loan/application/{loan_id}", response_model=SuccessOrFailResponse)
         async def withdraw_loan_application(loan_id: int, user = Depends(get_user_token)):
-            return {"Hello": "World"}
+            borrower = "123"  # TODO: get from KYC
+            try:
+                # get loan for id
+                results = loan_application_reader.get_loan_applications_for_borrower(borrower)
+                print(results)
+
+                # # make the update and write it to IPFS
+                # loan_application_writer = LoanApplicationWriter(ipfsclient, borrower, asking)
+                # loan_application_writer.
+                # loan_application_writer.write()
+            except Exception as e:
+                return SuccessOrFailResponse(
+                    success=False,
+                    error_message=e
+                )
+
+            return results
 
         # Loan endpoints
 

c66600de311e33a90ee8afddc5aee55542627997
Nate Schultz <nate.schultz@outlook.com>
2023-04-22 20:38:13 -0400

    worked on loanrouter

--- src/main.py
+++ src/main.py
@@ -1,25 +1,19 @@
-from typing import Union, List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import firebase_admin
-from firebase_admin import auth, credentials, initialize_app
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import Depends, HTTPException, status, Response
+from firebase_admin import credentials, initialize_app
 from dotenv import load_dotenv
-from dataclasses import dataclass, asdict
-from dacite import from_dict
-from typing import Union, TypeVar, Generic
-from pydantic import BaseModel
-from enum import Enum
 from pathlib import Path
 import os
-import schemas
-import utils
-import sumsub
+import src.utils
+from src.routes.loan import LoanRouter
 
-# server setup
 app = FastAPI()
 
+# class Server():
+
+#     @staticmethod
+#     def startup():
+#         # server setup
 origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
@@ -38,25 +32,13 @@ app.add_middleware(
 )
 
 # firebase auth credentials
-load_dotenv(Path(os.environ['SECRETS_PATH']+"/.env.nanoswap"))
+load_dotenv(Path(os.environ['SECRETS_PATH']+"/.env.nanobank"))
 initialize_app(credential=credentials.Certificate(os.environ['FIREBASE_CREDENTIALS_PATH']))
 
 # feature store config
-config = utils.get_config()
+config = src.utils.get_config()
 
+# add loan routes
+LoanRouter(app)
 
-# class TokenResponse(BaseModel):
-#     token: str
-# 
-# WIP
-# """ sumsub token for kyc and verification """
-# @app.get("/sumsub", response_model=TokenResponse)
-# async def get_sumsub_token(user = Depends(utils.get_user_token)):
-#     applicant_id = sumsub.create_applicant(user['uid'], 'basic-kyc-level')
-#     image_id = sumsub.add_document(applicant_id)
-#     status = sumsub.get_applicant_status(applicant_id)
-#     token = sumsub.get_access_token(user['uid'], 'basic-kyc-level')
-#     return {
-#         "token": token
-#     }
-
+# Server.startup()
--- src/routes/loan.py
+++ src/routes/loan.py
@@ -1,34 +1,35 @@
+from fastapi import Depends
+from typing import List
+from src.schemas import SuccessOrFailResponse
+from src.utils import get_user_token
 
-# Loan application endpoints
+class LoanRouter():
 
-@app.post("/loan/application", response_model=SuccessOrFailResponse)
-async def submit_loan_application(application: schemas.Loan, user = Depends(utils.get_user_token)):
-    return {"Hello": "World"}
+    def __init__(self, app):
 
-@app.delete("/loan/application/{loan_id}", response_model=SuccessOrFailResponse)
-async def withdraw_loan_application(loan_id: int, user = Depends(utils.get_user_token)):
-    return {"Hello": "World"}
+        # Loan application endpoints
 
+        @app.post("/loan/application", response_model=SuccessOrFailResponse)
+        async def submit_loan_application(user = Depends(get_user_token)):
+            return {"Hello": "World"}
 
-# Loan endpoints
-
-# TODO
-@app.get("/loans", response_model=List[schemas.Loan])
-async def get_all_loans(user = Depends(utils.get_user_token)):
-    return {"Hello": "World"}
+        @app.delete("/loan/application/{loan_id}", response_model=SuccessOrFailResponse)
+        async def withdraw_loan_application(loan_id: int, user = Depends(get_user_token)):
+            return {"Hello": "World"}
 
 
-# TODO
-@app.get("/loans/user", response_model=List[schemas.Loan])
-async def get_my_loans(user = Depends(utils.get_user_token)):
-    return {"Hello": "World"}
+        # Loan endpoints
 
-# WIP
-@app.get("/loan/{loan_id}", response_model=Union[schemas.Loan, SuccessOrFailResponse])
-async def get_loan_details(loan_id: int, user = Depends(utils.get_user_token)):
-    # loan = loan_ref.where(u'loan_id', u'==', loan_id).get()
-    # if loan.exists:
-    #     return { "status": {"success": True}, "data": loan.to_dict() }
-    
-    # return { "status": {"success": False, "error_code": 404, "error_message": "loan not found"} }
-    return {"Hello": "World"}
+        @app.get("/loans")
+        async def get_all_loans(user = Depends(get_user_token)):
+            return {"Hello": "World"}
+
+
+        @app.get("/loans/user")
+        async def get_my_loans(user = Depends(get_user_token)):
+            return {"Hello": "World"}
+
+        # WIP
+        @app.get("/loan/{loan_id}") #response_model=Union[schemas.Loan, SuccessOrFailResponse])
+        async def get_loan_details(loan_id: int, user = Depends(get_user_token)):
+            return {"Hello": "World"}

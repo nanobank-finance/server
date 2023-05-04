6f00eb63d6b88e4e26fe7dc8645c9e15515bf070
Nate Schultz <nate.schultz@outlook.com>
2023-05-04 11:09:10 -0400

    added a couple loan application tests

--- tests/integration/test_application_routes.py
+++ tests/integration/test_application_routes.py
@@ -29,3 +29,43 @@ async def test_submit_loan_application(client: AsyncClient, user_token: Dict[str
     # Then
     assert response.status_code == 200
     assert response.json() == {"success": True}
+
+
+@pytest.mark.asyncio
+async def test_get_all_loan_applications(client: AsyncClient, user_token: Dict[str, str]):
+    # Given
+    recent = False
+
+    # When
+    response = await client.get("/loan/application", params={"recent": recent}, headers=user_token)
+
+    # Then
+    assert response.status_code == 200
+    assert "results" in response.json()
+
+
+@pytest.mark.asyncio
+async def test_get_my_loan_applications(client: AsyncClient, user_token: Dict[str, str]):
+    # Given
+    recent = False
+
+    # When
+    response = await client.get("/loan/application/user/self", params={"recent": recent}, headers=user_token)
+
+    # Then
+    assert response.status_code == 200
+    assert "results" in response.json()
+
+
+@pytest.mark.asyncio
+async def test_get_their_loan_applications(client: AsyncClient, user_token: Dict[str, str]):
+    # Given
+    recent = False
+    them = 123
+
+    # When
+    response = await client.get("/loan/application/user/other", params={"recent": recent, "them": them}, headers=user_token)
+
+    # Then
+    assert response.status_code == 200
+    assert "results" in response.json()

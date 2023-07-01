488b86360054fcebe4571e2924cd064268927ca2
Nate Schultz <nate.schultz@outlook.com>
2023-07-01 11:09:34 -0400

    initial iteration

--- src/routes/sumsub.py
+++ src/routes/sumsub.py
@@ -1,12 +1,16 @@
 """Sumsub Routes."""
+from http.client import HTTPException
 from fastapi import Depends
 from fastapi import FastAPI
 from typing import Self
+
+from google.cloud import firestore
 
 from src.schemas import SuccessOrFailureResponse
 from src.utils import RouterUtils
 from src.sumsub import create_applicant, get_applicant_status, get_access_token
 
+db = firestore.Client()
 
 class SumsubRouter():
 
@@ -25,21 +29,32 @@ class SumsubRouter():
             Returns:
                 SuccessOrFailureResponse: `success=True` when successful.
             """
+
             # check if user id is "locked" in firestore
-            pass
+            user_ref = db.collection('users').document(user)
+            user_doc = user_ref.get()
+            if not user_doc.exists or user_doc.get('locked'):
+                raise HTTPException(status_code=400, detail="User is locked or does not exist")
 
             # "lock" user id mapping to prevent race conditions
-            pass
+            user_ref.update({'locked': True})
 
             # get applicant id corresponding to user from firestore
-            pass
+            applicant_id = user_doc.get('applicant_id')
 
             # if the applicant id does not exist, create a new one in sumsub
             # and store the id in firestore
-            pass
+            if not applicant_id:
+                applicant_id = create_applicant(user)
+                user_ref.update({'applicant_id': applicant_id})
 
             # query sumsub for the status of the applicant id
-            pass
+            status = get_applicant_status(applicant_id)
+
+            # Unlock user
+            user_ref.update({'locked': False})
+
+            return status
 
         @app.post("/onboard/start", response_model=SuccessOrFailureResponse)
         async def start_onboarding(

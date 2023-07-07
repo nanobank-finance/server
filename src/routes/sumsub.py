52366ad25e3c806e8943bf636c815bd27067ce1b
Nate Schultz <nate.schultz@outlook.com>
2023-07-07 18:34:54 -0400

    removed unused endpoint

--- src/routes/sumsub.py
+++ src/routes/sumsub.py
@@ -83,20 +83,3 @@ class SumsubRouter():
             applicant_id = user_data.get('applicant_id')
             LOG.debug(f"Applicant ID for user {user['uid']} is {applicant_id}")
             return applicant_id
-
-        @app.post("/onboard/start", response_model=SuccessOrFailureResponse)
-        async def start_onboarding(
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> SuccessOrFailureResponse:
-            """Start the onboarding process.
-
-             - Create a KYC applicant
-             - Store the Auth user ID with the KYC applicant ID
-
-            Args:
-                user (str): The user to submit the application for.
-
-            Returns:
-                SuccessOrFailureResponse: `success=True` when successful.
-            """
-            pass

eaccde83f88fe43d4340488d44f35150a7fab8a7
Nate Schultz <nate.schultz@outlook.com>
2023-08-22 20:47:47 -0400

    :poop: loan router cleanup :poop:

--- src/routes/loan.py
+++ src/routes/loan.py
@@ -40,13 +40,13 @@ class LoanRouter():
         secret_manager = SecretManager()
 
         @app.get(
-            "/loans/open",
-            response_model=dict
+            "/loans",
+            response_model=List[LoanResponse]
         )
-        async def get_open_loans(
+        async def get_all_loans(
             recent: bool = False,
             user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
+        ) -> List[LoanResponse]:
             """Get all open loans.
 
             Args:
@@ -57,261 +57,13 @@ class LoanRouter():
             Returns:
                 List: List of loans.
             """
-            return loan_reader.query_for_status(
-                LoanStatusType.PENDING_ACCEPTANCE,
-                recent_only=recent
-            ).to_dict(orient="records")
-
-        @app.get(
-            "/loans/accepted",
-            response_model=dict
-        )
-        async def get_accepted_loans(
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all accepted loans.
-
-            Args:
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            return loan_reader.query_for_status(
-                LoanStatusType.ACCEPTED,
-                recent_only=recent
-            ).to_dict(orient="records")
-
-        @app.get(
-            "/loans/user/self/open",
-            response_model=List[LoanResponse]
-        )
-        async def get_my_open_loans(
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List[LoanResponse]:
-            """Get all open loans for the user.
-
-            Args:
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]  # TODO: handle invalid request properly (and make enum instead of str?)  # noqa: E501
-
-            results = loan_reader.query_for_status(
-                LoanStatusType.PENDING_ACCEPTANCE,
-                index={
-                    perspective: user
-                }
-            )
+            results = loan_reader.get_all_loans(recent_only=recent)
 
             LOG.debug("Results: %s", results)
-
-            return RouterUtils.sanitize_output(results.to_dict(orient="records"))
-
-        @app.get(
-            "/loans/user/self/draft",
-            response_model=List[LoanResponse]
-        )
-        async def get_my_draft_loan_offers(
-            perspective: str = "lender",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List[LoanResponse]:
-            """Get all open loans for the user.
-
-            Args:
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]  # TODO: handle invalid request properly (and make enum instead of str?)  # noqa: E501
-
-            results = loan_reader.query_for_status(
-                LoanStatusType.DRAFT,
-                index={
-                    perspective: user
-                }
-            )
-
-            LOG.debug("Results: %s", results)
-
-            return RouterUtils.sanitize_output(results.to_dict(orient="records"))
-
-        @app.get(
-            "/loans/user/their/open",
-            response_model=dict
-        )
-        async def get_their_open_loans(
-            them: int,
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all open loans for the user.
-
-            Args:
-                them (int): The user ID of the user.
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]
-            results = loan_reader.query_for_status(
-                LoanStatusType.PENDING_ACCEPTANCE,
-                index={
-                    perspective: them
-                },
-                recent_only=recent
-            )
-            return results.to_dict(orient="records")
-
-        @app.get(
-            "/loans/user/self/accepted",
-            response_model=dict
-        )
-        async def get_my_accepted_loans(
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all accepted loans for the user.
-
-            Args:
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]  # TODO: handle invalid request properly (and make enum instead of str?)  # noqa: E501
-            borrower = "123"  # TODO: get from KYC
-            results = loan_reader.query_for_status(
-                LoanStatusType.ACCEPTED,
-                index={
-                    perspective: borrower
-                },
-                recent_only=recent
-            )
-            return results.to_dict(orient="records")
-
-        @app.get(
-            "/loans/user/their/accepted",
-            response_model=dict
-        )
-        async def get_their_accepted_loans(
-            them: int,
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all accepted loans for the user.
-
-            Args:
-                them (int): The user ID of the user.
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]
-            results = loan_reader.query_for_status(
-                LoanStatusType.ACCEPTED,
-                index={
-                    perspective: them
-                },
-                recent_only=recent
-            )
-
-            return results.to_dict(orient="records")
-
-        @app.get(
-            "/loans/user/self",
-            response_model=dict
-        )
-        async def get_my_loans(
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all loans for the user.
-
-            Args:
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]  # TODO: handle invalid request properly (and make enum instead of str?)  # noqa: E501
-            borrower = "123"  # TODO: get from KYC
-            if perspective == "borrower":
-                results = loan_reader.query_for_borrower(borrower, recent_only=recent)
-            elif perspective == "lender":
-                results = loan_reader.query_for_lender(borrower, recent_only=recent)
-
-            return results.to_dict(orient="records")
-
-        @app.get(
-            "/loans/user/other",
-            response_model=dict
-        )
-        async def get_their_loans(
-            them: str,
-            perspective: str = "borrower",
-            recent: bool = False,
-            user: str = Depends(RouterUtils.get_user_token)
-        ) -> List:
-            """Get all loans for the user.
-
-            Args:
-                them (str): The user ID of the user.
-                perspective (str, optional): The perspective of the user.
-                    Defaults to "borrower".
-                recent (bool, optional): If True, only return the most recent
-                    loan. Defaults to False.
-                user (str, optional): User token. Defaults to Depends(RouterUtils.get_user_token).
-
-            Returns:
-                List: List of loans.
-            """
-            assert perspective in ["lender", "borrower"]  # TODO: handle invalid request properly (and make enum instead of str?)  # noqa: E501
-            if perspective == "borrower":
-                results = loan_reader.query_for_borrower(them)
-            elif perspective == "lender":
-                results = loan_reader.query_for_lender(them)
-
-            return results.to_dict(orient="records")
+            # results = uuid_images.add_uuid_images(results)
+            # results = RouterUtils.sanitize_output(results.to_dict(orient='records'))
+            LOG.debug("Final results: %s", results)
+            return results if not results.empty else []
 
         @app.get(
             "/loan",
@@ -422,6 +174,3 @@ class LoanRouter():
             # read the loan
             results = loan_reader.query_for_loan(loan_id)
             print(results)
-
-
-        # TODO: make payment
